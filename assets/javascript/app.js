b4b64b3f1f323fabf44daaa5264364c250fdf5d5
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-07-02 14:03:49 -0400

    Added variable to hold the array of all the questions as well as their respective answers.

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+//TRIVIA GAME
+//Variable to hold all possible questions and their respective answers.
+var options = [
+    {
+        question:"What color is the sky?",
+        choices: ["Red", "White", "Blue", "Green"],
+        answer: 3,
+        
+    },
+    {
+        question:"What country is Rawalpindi in?",
+        choices: ["Afghanistan", "Pakistan", "India", "Kansas"],
+        answer: 2,
+    },
+    {
+        question:"What is the compatable round for a standard issue M4 for the U.S. Army",
+        choices: [".308", "30-30", ".762", ".556"],
+        answer: 4,
+    },
+    {
+        question: "How many inches in 1 foot?",
+        choices: ["12", "14", "36", "6"],
+        answer: 1,
+    },
+    {
+        question: "What is the capital of Wisconsin?",
+        choices: ["Waukesha", "Milwaukee", "Madison", "Green Bay"],
+        answer: 3,
+    },
+    {
+        question: "How many players from each team start on the court for a basketball game?",
+        choices: ["5", "12", "7", "10"],
+        answer: 1,
+    },
+    {
+        question: "Complete this phrase: Red, Green, _____",
+        choices: ["Yellow", "Refactor", "White", "Recliner"],
+        answer: 2,
+    },
+    {
+        question: "Select what goes first from the choices below:",
+        choices: ["Git Push", "Git Pull", "Git Status", "Git Commit"],
+        answer: 3,
+    },
+    {
+        question: "Who was the lead actor in American Gangster?",
+        choices: ["Al Pacino", "Robert Downey Jr.", "Johnny Depp", "Denzel Washington"],
+        answer: 4,
+    },
+    {
+        question: "Who won the NBA finals this year?",
+        choices: ["Cleveland", "Raptors", "Golden State", "Lakers"],
+        answer: 2,
+    }
+];
